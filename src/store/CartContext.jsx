3cfe4f737ef6f56adc1e502485b2056982f0f4e0
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-17 14:25:56 +0300

    feat: add cart reducer

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,4 +1,6 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
+
+import { ADD_ITEM, REMOVE_ITEM} from '../utils/constants'
 
 const CartContext = createContext({
     items: [],
@@ -6,7 +8,37 @@ const CartContext = createContext({
     removeItem: (id) => {}
 })
 
+function cartReducer(state, action) {
+    switch (action.type) {
+        case ADD_ITEM:
+            const existingIndex = state.items.findIndex((item) => item.Id)
+            
+            const updatedItems = [...state.items]
+            if (existingIndex > -1) {
+                const existingItem = state.items[existingIndex]
+                const updatedItem = {
+                    ...existingItem,
+                    quantity: existingItem.quantity + 1
+                }
+                updatedItems[existingIndex] = updatedItem
+            } else {
+                updatedItems.push({
+                    ...action.item,
+                    quantity: 1
+                })
+            }
+            return {
+                ...state,
+                items : updatedItems
+            };
+    
+        default:
+            return state;
+    }
+}
+
 export function CartContextProvider({children}) {
+    const [state, dispatch] = useReducer(cartReducer, { items: [] })
     return <CartContext.Provider>
         {children}
     </CartContext.Provider>
