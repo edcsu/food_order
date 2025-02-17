3d7c87e8042cc4362378a632985bdc4404e87f0e
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-17 13:23:38 +0300

    feat: create meal item component

---
+++ src/components/MealItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function MealItem({ meal}) {
+  return (
+    <li className='meal-item'>
+        <article>
+            <img src={meal.image} alt={meal.name} />
+            <div>
+                <h3>{meal.name}</h3>
+                <p className='meatl-item-price'>{meal.price}</p>
+                <p className='meal-item-description'>{meal.description}</p>
+            </div>
+            <p className='mea-item-actions'>
+                <button>Add to Cart</button>
+            </p>
+        </article>
+    </li>
+  )
+}
+
+export default MealItem
