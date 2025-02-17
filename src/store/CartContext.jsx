1e6d65111a965dc9e5e8cc02f0e9aaea3d64561a
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-17 14:03:56 +0300

    feat: initial cart context

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,15 @@
+import { createContext } from 'react'
+
+const CartContext = createContext({
+    items: [],
+    addItem: (item) => {},
+    removeItem: (id) => {}
+})
+
+export function CartContextProvider({children}) {
+    return <CartContext.Provider>
+        {children}
+    </CartContext.Provider>
+}
+
+export default CartContext;
