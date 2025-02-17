113b0c8a179326d098d284b274d404997d9416cd
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-17 15:14:42 +0300

    style: show total number of cart items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,25 @@
+import { useContext } from 'react'
+
 import LogoImg from '../assets/logo.jpg'
 import Button from './UI/Button'
+
+import CartContext from '../store/CartContext'
+
 function Header() {
-  return (
+    const { items } = useContext(CartContext)
+    
+    const totalCartItems = items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity
+    }, 0)
+
+    return (
     <header id='main-header'>
         <div id='title'>
             <img src={LogoImg} alt="logo" />
             <h1>Ske Foods</h1>
         </div>
         <nav>
-            <Button textOnly>Cart (0)</Button>
+            <Button textOnly>Cart ({totalCartItems})</Button>
         </nav>
     </header>
   )
