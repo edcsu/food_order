9892513463f8eedb87cf60ef6b31ab5c7b662328
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-17 19:04:15 +0300

    style: add error component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -2,11 +2,26 @@ import { useEffect, useState } from 'react'
 import { BASE_URL } from '../utils/constants'
 import MealItem from './MealItem'
 import useHttp from '../hooks/useHttp'
+import ErrorPage from './Error'
 
 const requestConfig = {}
 
 function Meals() {
     const {data, isLoading, error} = useHttp(`${BASE_URL}/meals`, requestConfig, [])
+
+    if (isLoading) {
+        return (
+            <p className='center'>
+                Fetching meals...
+            </p>
+        )
+    }
+
+    if (error) {
+        return (
+            <ErrorPage title="Failed to get meals" message={error} />
+        )
+    }
 
     return (
         <ul id='meals'>
